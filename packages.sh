85e691b3ea2ce10ad8e530363399ce37bd600446
Jakub Štětina <37512119+techguy007@users.noreply.github.com>
2022-10-28 13:34:59 +0200

    added linux packages file

---
+++ packages.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+function install(){
+  sudo apt install -y $1;
+}
+
+
+# Python
+install python3
+
+# C programming
+install gcc
+install gdb
+install make
+
+# MULTIPLEXERS
+install screen
+install tmux
+
+# Processes
+install htop
+
+# Tar 
+install tar
+
+# Git 
+install git
+
+# Maps 
+install mapscii
+
+# NPM pack. manager
+install nodejs
+install npm
+
+# Wikipedia tool
+sudo npm install wikit -g
+
+
