ad615bb0e359e0063e8cc92f246bf6b48f7ebbbd
techguy007 <jakub.stetina2003@gmail.com>
2023-06-06 10:52:27 +0200

    fast auto commit

--- send_mail.sh
+++ send_mail.sh
@@ -57,7 +57,7 @@ fi
 
 
 # Send mail
-echo "$MAIL_BODY" | mutt -s "$HOST_NAME: $MAIL_SUBJECT" "$RECIPIENT_MAIL"
+echo "$MAIL_BODY" | mutt -s "$HOSTNAME: $MAIL_SUBJECT" "$RECIPIENT_MAIL"
 if [ $? -eq 0 ];
 then 
     echo "Mail sent successfuly"
