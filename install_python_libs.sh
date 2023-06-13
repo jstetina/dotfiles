349235ed3244ff44d8504ca647af3bffe3711a02
Jakub Štětina <37512119+startler03@users.noreply.github.com>
2023-06-13 21:01:51 +0200

    Update install_python_libs.sh

--- install_python_libs.sh
+++ install_python_libs.sh
@@ -1,11 +1,32 @@
-pip install bs4;
-pip install selenium;
-pip install requests;
-pip install urllib;
-pip install json;
-pip install pushbullet.py
-pip install numpy;
-pip install pandas;
-pip install validators
-pip install re;
+#! /bin/bash
+PYTHON_VERSION=$(python --version)
+PYTHON_3_11=0
 
+sudo apt install python3 python-is-python3 -y
+
+# Check python version
+if [[ $(echo "$PYTHON_VERSION" | grep 3.11) ]]
+then
+    sudo apt install pipx python3-full -y
+    PYTHON_3_11=1
+fi
+
+install(){
+    if [[ $PYTHON_3_11 -eq 1 ]]
+    then
+        pipx install "$1"
+    else
+        pip install "$1"
+    fi
+}
+
+install bs4;
+install selenium;
+install requests;
+install urllib;
+install json;
+install pushbullet.py
+install numpy;
+install pandas;
+install validators
+install re;
