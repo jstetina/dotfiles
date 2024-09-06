f2ff2382a465a3e85724a28a3e1689254a511202
jstetina <jakub.stetina2003@gmail.com>
2024-09-06 10:52:02 +0200

    fast auto commit

--- scripts/custom_installs/install_cmake.sh
+++ scripts/custom_installs/install_cmake.sh
@@ -3,5 +3,5 @@ cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")"
 source install_dir.sh
 
 git clone https://github.com/Kitware/CMake.git $INSTALL_DIR/cmake --depth 1 --branch release
-cd $HOME/custom/apps/cmake
+cd $INSTALL_DIR/cmake
 ./configure && make && make -j$(nproc) && make install
