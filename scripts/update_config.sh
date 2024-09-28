9084621ce106df0efe0e0c33ab8d14b5080b7855
jstetina <jakub.stetina2003@gmail.com>
2024-09-28 15:00:43 +0200

    fast auto commit

--- scripts/update_config.sh
+++ scripts/update_config.sh
@@ -42,7 +42,12 @@ cp $OTHER_DIR/vimrc ~/.vimrc
 echo "Updated vim config"
 
 # neovim
-cp -r $OTHER_DIR/nvim $HOME/.config
+if [ ! -d "$HOME/.config/nvim" ]; then 
+    echo "neovim config not detected, downloading.."
+    git clone https://github.com/NvChad/starter $HOME/.config/nvim --depth 1 
+else 
+    cp -r "$OTHER_DIR/nvim" "$HOME/.config"
+fi
 echo "Updated nvim config"
 
 cp $OTHER_DIR/tmux.conf ~/.tmux.conf
