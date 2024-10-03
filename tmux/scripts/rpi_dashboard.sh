f3f84de1bbe77c61d9cf6bba836ddcbc8ac232d7
jstetina <jakub.stetina2003@gmail.com>
2024-10-03 18:27:39 +0200

    fast auto commit

--- tmux/scripts/rpi_dashboard.sh
+++ tmux/scripts/rpi_dashboard.sh
@@ -12,9 +12,9 @@ tmux split-window -v -t $SESSION_NAME:0
 tmux select-pane -t $SESSION_NAME:0.1
 tmux split-window -h -t $SESSION_NAME:0.1
 
-tmux send-keys -t $SESSION_NAME:0.0 'ssh triton' Enter 
-tmux send-keys -t $SESSION_NAME:0.1 'ssh ironman' Enter
-tmux send-keys -t $SESSION_NAME:0.2 'ssh tantalus' Enter
+tmux send-keys -t $SESSION_NAME:0.0 'ssh triton; clear' Enter 
+tmux send-keys -t $SESSION_NAME:0.1 'ssh ironman; clear' Enter
+tmux send-keys -t $SESSION_NAME:0.2 'ssh tantalus; clear' Enter
 
 tmux attach -t $SESSION_NAME
 
