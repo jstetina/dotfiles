a92dc20818c595a8629d3df2f704eac87619207b
jstetina <jakub.stetina2003@gmail.com>
2024-10-03 18:25:44 +0200

    fast auto commit

--- tmux/scripts/rpi_dashboard.sh
+++ tmux/scripts/rpi_dashboard.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Creates a tmux session with one windows and three panes for rpi managing
+
 SESSION_NAME="RPI_dashboard"
+
+tmux kill-session -t $SESSION_NAME 2> /dev/null # Kill previous session if exists
 
 tmux new-session -d -s $SESSION_NAME
 
 tmux split-window -v -t $SESSION_NAME:0 
-
 tmux select-pane -t $SESSION_NAME:0.1
 tmux split-window -h -t $SESSION_NAME:0.1
 
