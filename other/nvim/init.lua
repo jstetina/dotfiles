09530ced918f4baf07125324ff6d09f47566ca50
jstetina <jakub.stetina2003@gmail.com>
2024-10-20 08:34:49 +0200

    fast auto commit

--- other/nvim/init.lua
+++ other/nvim/init.lua
@@ -1,2 +1,34 @@
 require("config.core")
 require("config.lazy")
+
+-- Set shift width based on file type
+vim.api.nvim_create_augroup("FileTypeShiftWidth", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  command = "setlocal shiftwidth=4",
+  group = "FileTypeShiftWidth",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "javascript,typescript,javascriptreact,typescriptreact",
+  command = "setlocal shiftwidth=2",
+  group = "FileTypeShiftWidth",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "lua",
+  command = "setlocal shiftwidth=2",
+  group = "FileTypeShiftWidth",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "html,css,scss",
+  command = "setlocal shiftwidth=2",
+  group = "FileTypeShiftWidth",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  command = "setlocal shiftwidth=4",
+  group = "FileTypeShiftWidth",
+})
